7a0061ddc035308e8e9706d3f0289bf589add6b7
Baz Rahimi <bazrahimi@hotmail.com>
2023-06-15 05:02:41 +1000

    addes css and DOM elements

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+//DOM Elements
+const startBtn = document.getElementById('start-button');
+const timerEl = document.getElementById('timer');
+const questionEl = document.getElementById('question');
+const choicesEl = document.getElementById('choices');
+const feedbackEl = document.getElementById('feedback');
+const initialsInput = document.getElementById('initials');
+const submitBtn = document.getElementById('submit-button');
