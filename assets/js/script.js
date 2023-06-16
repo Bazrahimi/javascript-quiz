7999008c6ed4586a4b1d1a61a673644062bb5866
Baz Rahimi <bazrahimi@hotmail.com>
2023-06-17 03:37:17 +1000

    remove .

--- assets/js/script.js
+++ assets/js/script.js
@@ -165,7 +165,7 @@ function endQuiz() {
     } else {
       saveScore();
 
-      window.location.href = '../assets/html/scores.html';
+      window.location.href = './assets/html/scores.html';
    
     }
   });
